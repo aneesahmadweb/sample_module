9e31da70c3597709fa3e3912ea43a5df44081f47
Anees Ahmad <aneesahmadweb@gmail.com>
2023-03-28 05:19:38 +0500

    one

---
+++ app/code/Sample/Mod/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *  All rights reserved.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Sample_Mod',
+    __DIR__
+);
